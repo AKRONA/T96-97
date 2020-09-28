2a207011d9f637c53bdcef52367315749de5a9e9
AKRONA <fedorov2000000@gmail.com>
2020-09-28 22:18:26 +0300

    96

--- akrona/akrona.cpp
+++ akrona/akrona.cpp
@@ -1,34 +1,10 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void task96();
 void task97();
-int factorial(int n)
-{
-    if (n > 1)
-        return n * factorial(n - 1);
-    else
-        return 1;
-}
-
-int task(bool arr[], int n, int k = 1) {
-
-    int c = 0;
-    if (k <= n) {
-        for (int i = 1; i <= n; i++) {
-            if ((i != k) && !arr[i - 1]) {
-                arr[i - 1] = true;
-                c += task(arr, n, k + 1);
-                arr[i - 1] = false;
-            }
-        }
-    }
-    else {
-        c = 1;
-    }
-
-    return c;
-}
 
 int main() {
     setlocale(0, "");
@@ -38,29 +14,36 @@ int main() {
 
 void task96() {
     unsigned int n;
-    cout << "задача шарики\n введите n";
+    cout << "задача 96\n введите четырехзначное число\n";
     cin >> n;
-    bool* arr = new bool[n];
-
-    for (int i = 0; i < n; i++) {
-        arr[i] = false;
+    srand(time(0)); //рандом зависящий от времени
+    int r = rand()%10000;
+    cout << "рандомное число равно =";
+    if (r<10) {
+        cout << "000";
     }
-    cout << "ответ равен " << factorial(n) - task(arr, n);
+    else if(r<100){
+        cout << "00";
+    }
+    else if (r < 1000) {
+        cout << "0";
+    }
+    cout << r << endl;
+    int l = 1, p = 0, m = 0, buf = 4;
+    while (r>0 && n>0) {
+        if (r % 10 == n % 10) {
+            p++;
+        }
+        else {
+            m++;
+        }
+        n = n / 10;
+        r = r / 10;
+        buf--;
+    }
+    cout << "плюсы " << p << "   минусы " << m << endl;
 }
 
 void task97() {
-    cout << "\n\nзадача спинерbl\nвведите A, B, C\n";
-    int a, b, c, s = 0, l = 0;
-    cin >> a >> b >> c;
-    while (s <= c) {
-        l++;
-        s = a + b * l;
-    }
-    l--;
-    if (l < 1) {
-        cout << "нет лопастей";
-    }
-    else {
-        cout << l;
-    }
+
 }
